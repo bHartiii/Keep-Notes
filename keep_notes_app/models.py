08dddf9f041eeaa9253667ae09c4551e86d750f6
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-24 16:59:08 +0530

    Bharti-ADD : Error resolved in sending mail

--- keep_notes_app/models.py
+++ keep_notes_app/models.py
@@ -29,7 +29,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255, unique=True,  db_index=True)
     email = models.EmailField(max_length=255, unique=True,  db_index=True)
     is_verified = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- keep_notes_app/views.py
+++ keep_notes_app/views.py
@@ -23,9 +23,10 @@ class RegisterView(generics.GenericAPIView):
         current_site = get_current_site(request).domain
         relativeLink= reverse('email-verify')
        
-        absurl = 'http://'+current_site+relativeLink+"?token"+str(token)
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
         email_body = 'Hii'+user.username+' Use this below to verify your email \n'+absurl
-        data = {'email_body':email_body ,'to_email':user.email, 'email_subject':'Verify you email'} 
+        data = {'email_body':email_body ,'to_email':user.email, 'email_subject':'Verify you email'}
+        Util.send_email(data) 
         return Response(user_data,status=status.HTTP_201_CREATED)
 
 class VerifyEmail(generics.GenericAPIView):
