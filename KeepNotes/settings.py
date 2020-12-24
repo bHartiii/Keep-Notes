c153e7719aa17ff8f010b29f340a59a0d1cce653
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-24 18:05:16 +0530

    Bharti-ADD : Swagger api is added

--- KeepNotes/settings.py
+++ KeepNotes/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_swagger',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- KeepNotes/urls.py
+++ KeepNotes/urls.py
@@ -15,19 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from rest_framework_swagger.views import get_swagger_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-schema_view = get_swagger_view(title='Keep Notes API')
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="KeepNotesApp API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.ourapp.com/policies/terms/",
+      contact=openapi.Contact(email="contact@keepNotes.local"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
     
 
 urlpatterns = [
-    path('swagger-docs/', schema_view),
+    
     path('admin/', admin.site.urls),
     path('auth/',include('keep_notes_app.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(),name='token_verify')
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
