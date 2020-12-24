60174ed31b147923f4668e76ef095a605baa44ed
malibharti5@gmail.com <malibharti5@gmail.com>
2020-12-24 17:38:33 +0530

    Bharti-ADD : Tooken verification method is added in views

--- keep_notes_app/views.py
+++ keep_notes_app/views.py
@@ -7,6 +7,8 @@ from .models import User
 from .utils import Util
 from django.contrib.sites.shortcuts import  get_current_site
 from django.urls import reverse
+from django.conf import settings
+import jwt
 
 class RegisterView(generics.GenericAPIView):
 
@@ -21,7 +23,7 @@ class RegisterView(generics.GenericAPIView):
         user = User.objects.get(email=user_data['email'])
         token = RefreshToken.for_user(user).access_token
         current_site = get_current_site(request).domain
-        relativeLink= reverse('email-verify')
+        relativeLink = reverse('email-verify')
        
         absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
         email_body = 'Hii'+user.username+' Use this below to verify your email \n'+absurl
@@ -30,5 +32,18 @@ class RegisterView(generics.GenericAPIView):
         return Response(user_data,status=status.HTTP_201_CREATED)
 
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token,settings.SECRET_KEY)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_active=True
+                user.is_verified=True
+                user.save()
+            return Response({'email':'Succefully Activated'},status=status.HTTP_200_OK)
+
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error':'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error':'Invalid Token'}, status=status.HTTP_400_BAD_REQUEST)
