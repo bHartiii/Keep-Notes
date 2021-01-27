41d242afc52bc03ff66e7baccbb5a1c65832e6d2
malibharti5@gmail.com <malibharti5@gmail.com>
2021-01-27 11:55:06 +0530

    Bharti-ADD : Enviornment variable configuration is added

--- KeepNotes/settings.py
+++ KeepNotes/settings.py
@@ -151,8 +151,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('mail_username')
-EMAIL_HOST_PASSWORD = os.environ.get('mail_password')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 LOGGING = {
     'version':1, 
